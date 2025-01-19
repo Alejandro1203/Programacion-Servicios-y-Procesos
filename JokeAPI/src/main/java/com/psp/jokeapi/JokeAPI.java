0f39f3206fcbcc48622ebc1f29d1cb506e23eff2
Alejandro1203 <alefg2003@gmail.com>
2025-01-19 14:33:57 +0100

    JokeAPI

--- JokeAPI/src/main/java/com/psp/jokeapi/JokeAPI.java
+++ JokeAPI/src/main/java/com/psp/jokeapi/JokeAPI.java
@@ -1,22 +1,62 @@
 package com.psp.jokeapi;
-
-import com.google.gson.Gson; 
-import com.google.gson.GsonBuilder;  
+  
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 /**
  *
  * @author Alejandro
  */
 public class JokeAPI {
+    
+    public static String getJoke(String url) {
+        String joke = ""; 
+        
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(url))
+                .build();
+        
+        HttpClient httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .build();
+        
+        try {
+            HttpResponse<String> response = httpClient.send(request,
+                HttpResponse.BodyHandlers.ofString());
+            
+            if(response.statusCode() == 200) {
+                ObjectMapper mapper = new ObjectMapper();
+                JsonNode root = mapper.readTree(response.body());
+                joke = root.get("joke").asText();
+                
+            } else {
+                joke = "ERROR al encontrar el chiste " + response.statusCode();
+            }
+            
+        } catch(IOException | InterruptedException e) {
+            System.out.println(e);
+        }
+        
+        return joke;
+    }
 
     public static void main(String[] args) {
-        String jsonString = "{\"name\":\"Mahesh\", \"age\":21}";
-
-        GsonBuilder builder = new GsonBuilder();
-        builder.setPrettyPrinting();
-
-        Gson gson = builder.create();
-//        Student student = gson.fromJson(jsonString, Student.class);
-
+        String esquema = "https://v2.";
+        String servidor = "jokeapi.dev/joke/";
+        String categoria = "Programming?";
+        String lenguaje = "lang=es";
+        String tipo_broma = "type=single";
+        String separador = "&";
+        
+        String url = esquema + servidor + categoria + lenguaje + separador + tipo_broma;
+        
+        System.out.println(getJoke(url));
     }
 }
