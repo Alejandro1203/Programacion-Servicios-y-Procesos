60842afa548502fe8a67b80a16df72327f63b5d1
Alejandro1203 <alefg2003@gmail.com>
2025-02-05 11:49:02 +0100

    Interfaz Autentificacion

---
+++ TEMA 4/InterfazAutentificacion/src/interfazautentificacion/InterfaceManager.java
@@ -0,0 +1,31 @@
+package interfazautentificacion;
+
+import java.awt.Color;
+import java.awt.Label;
+import java.io.File;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class InterfaceManager {
+    
+    public static boolean ficheroExiste(String ruta) {
+        boolean existe = false;
+        
+        File archivo = new File(ruta);
+        
+        if(archivo.exists()) {
+            existe = true;
+        }
+        
+        return existe;
+    }
+    
+    public static void setLabel(Label label, Color color, String texto) {
+        label.setForeground(color);
+        label.setText(texto);
+    }
+    
+    
+}
